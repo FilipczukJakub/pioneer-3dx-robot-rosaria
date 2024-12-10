ec5ba7aaa8f64f1f3d3f137a6d2d4952307e4ad5
f_jakub <filipczukjakub@gmail.com>
2024-12-10 18:24:53 +0100

    test

--- RosAria.cpp
+++ RosAria.cpp
@@ -107,7 +107,7 @@ class RosAriaNode
     ArRobotConnector *conn;
     ArLaserConnector *laserConnector;
     ArRobot *robot;
-    ArGripper::ArGripper gripper;
+    ArGripper gripper;
     nav_msgs::Odometry position;
     rosaria::BumperState bumpers;
     ArPose pos;
@@ -365,6 +365,7 @@ int RosAriaNode::Setup()
   // called once per instance, and these objects need to persist until the process terminates.
 
   robot = new ArRobot();
+  gripper = new ArGripper(robot&)
   ArGripper::ArGripper gripper = new ArGripper::ArGripper(robot);
   ArArgumentBuilder *args = new ArArgumentBuilder(); //  never freed
   ArArgumentParser *argparser = new ArArgumentParser(args); // Warning never freed
