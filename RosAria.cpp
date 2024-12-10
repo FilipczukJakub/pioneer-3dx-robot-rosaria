82d15abf20e662ae0a6e2dca2b3900cd11c6304b
f_jakub <filipczukjakub@gmail.com>
2024-12-10 18:28:41 +0100

    test

--- RosAria.cpp
+++ RosAria.cpp
@@ -365,7 +365,7 @@ int RosAriaNode::Setup()
   // called once per instance, and these objects need to persist until the process terminates.
 
   robot = new ArRobot();
-  gripper = new ArGripper(robot&)
+  gripper = new ArGripper(robot)
   ArGripper::ArGripper gripper = new ArGripper::ArGripper(robot);
   ArArgumentBuilder *args = new ArArgumentBuilder(); //  never freed
   ArArgumentParser *argparser = new ArArgumentParser(args); // Warning never freed
@@ -765,7 +765,7 @@ void RosAriaNode::cmdvel_watchdog(const ros::TimerEvent& event)
 void RosAriaNode::gripper_cb( const int &msg)
 {
   // ArGripper::ArGripper gripper(robot);
-  ArGripper::ArGripper gripper = gripperArGripper::ArGripper(robot);
+  // ArGripper::ArGripper gripper = gripperArGripper::ArGripper(robot);
   ROS_INFO( "gripper action: [%d]", msg );
   switch(msg){
 	  case 0:
